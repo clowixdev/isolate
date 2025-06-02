f032c95d7f77398c0ae984e5d340287026d6a5a1
Nikita Mishenev <clowixdev@gmail.com>
2025-06-02 22:50:06 +0300

    added MNT and PID namespaces isolation

--- src/isolate.cpp
+++ src/isolate.cpp
@@ -8,6 +8,9 @@ Mishenev Nikita 5131001/30002
 #include <stdarg.h>
 #include <sys/prctl.h>
 #include <wait.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <syscall.h>
 
 #include <iostream>
 #include <fstream>
@@ -19,6 +22,9 @@ using namespace std;
 #define DEFAULT_UID     (1000)
 
 static char cmd_stack[STACKSIZE];
+
+void setup_mns(const char *);
+void prepare_procfs(void);
 
 struct params {
     int fd[2];
@@ -64,6 +70,8 @@ int cmd_exec(void *arg) {
 
     params *params = (struct params *)arg;
     await_setup(params->fd[0]);
+
+    setup_mns("mini_rootfs");
 
     if (setgid(0) == -1) {
         kill_thread("Failed to setgid: %m\n");
@@ -118,6 +126,44 @@ void setup_uns(int pid) {
     gid_mapper.close();
 }
 
+void setup_mns(const char *rootfs) {
+    const char *mnt = rootfs;
+
+    if (mount(rootfs, mnt, "ext4", MS_BIND, "")) {
+        kill_thread("Failed to mount %s at %s: %m\n", rootfs, mnt);
+    }
+
+    if (chdir(mnt)) {
+        kill_thread("Failed to chdir to rootfs mounted at %s: %m\n", mnt);
+    }
+
+    const char *old_fs = ".old_fs";
+
+    if (mkdir(old_fs, 0777) && errno != EEXIST) {
+        kill_thread("Failed to mkdir old_fs %s: %m\n", old_fs);
+    }
+
+    if (syscall(SYS_pivot_root, ".", old_fs)) {
+        kill_thread("Failed to pivot_root from %s to %s: %m\n", rootfs, old_fs);
+    }
+
+    if (chdir("/")) {
+        kill_thread("Failed to chdir to new root: %m\n");
+    }
+
+    if (mkdir("/proc", 0555) && errno != EEXIST) {
+        kill_thread("Failed to mkdir /proc: %m\n");
+    }
+
+    if (mount("proc", "/proc", "proc", 0, "")) {
+        kill_thread("Failed to mount proc: %m\n");
+    }
+
+    if (umount2(old_fs, MNT_DETACH)) {
+        kill_thread("Failed to umount old_fs %s: %m\n", old_fs);
+    }
+}
+
 int main(int argc, char **argv) {
     params params = {0, 0};
     parse_args(argc, argv, &params);
@@ -126,7 +172,7 @@ int main(int argc, char **argv) {
         kill_thread("Failed to create pipe: %m");
     }
 
-    int clone_flags = SIGCHLD | CLONE_NEWUTS | CLONE_NEWUSER;
+    int clone_flags = SIGCHLD | CLONE_NEWUTS | CLONE_NEWUSER | CLONE_NEWNS | CLONE_NEWPID;
     int cmd_pid = clone(cmd_exec, cmd_stack + STACKSIZE, clone_flags, &params);
 
     if (cmd_pid < 0) {
